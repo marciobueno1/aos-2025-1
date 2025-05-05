b1cb05cb0b10d06f483c9b7dbc29319dffffed19
Marcio Bueno <marciobueno@gmail.com>
2025-05-05 19:23:05 -0300

    refatorando api/index.js: criando as rotas (routes)

--- express-01/api/index.js
+++ express-01/api/index.js
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
 
 import models from "./models";
+import routes from "./routes";
 
 const app = express();
 
@@ -28,86 +28,14 @@ app.use((req, res, next) => {
   next();
 });
 
-// handlers
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
-
-app.get("/session", (req, res) => {
-  return res.send(req.context.models.users[req.context.me.id]);
-});
+// rotas
+app.use("/session", routes.session);
+app.use("/users", routes.user);
+app.use("/messages", routes.message);
 
 app.get("/", (req, res) => {
-  return res.send("Received a GET HTTP method");
+  return res.send("Hello Express!");
 });
-
-app.post("/", (req, res) => {
-  return res.send("Received a POST HTTP method");
-});
-
-app.put("/", (req, res) => {
-  return res.send("Received a PUT HTTP method");
-});
-
-app.delete("/", (req, res) => {
-  return res.send("Received a DELETE HTTP method");
-});
-
-app.get("/users", (req, res) => {
-  return res.send(Object.values(req.context.models.users));
-});
-
-app.get("/users/:userId", (req, res) => {
-  return res.send(req.context.models.users[req.params.userId]);
-});
-
-app.post("/users", (req, res) => {
-  return res.send("POST HTTP method on user resource");
-});
-
-app.put("/users/:userId", (req, res) => {
-  return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
-});
-
-app.delete("/users/:userId", (req, res) => {
-  return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
-});
-
-// messages
-app.get("/messages", (req, res) => {
-  return res.send(Object.values(req.context.models.messages));
-});
-
-app.get("/messages/:messageId", (req, res) => {
-  return res.send(req.context.models.messages[req.params.messageId]);
-});
-
-app.post("/messages", (req, res) => {
-  const id = uuidv4();
-  const message = {
-    id,
-    text: req.body.text,
-    userId: req.context.me.id,
-  };
-
-  req.context.models.messages[id] = message;
-
-  return res.send(message);
-});
-
-app.delete("/messages/:messageId", (req, res) => {
-  const { [req.params.messageId]: message, ...otherMessages } =
-    req.context.models.messages;
-  req.context.models.messages = otherMessages;
-  return res.send(message);
-});
-
-// Outro código para realizar a remoção do objeto
-// app.delete("/messages/:messageId", (req, res) => {
-//   const message = req.context.models.messages[req.params.messageId];
-//   delete req.context.models.messages[req.params.messageId];
-//   return res.send(message);
-// });
 
 const port = process.env.PORT ?? 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
